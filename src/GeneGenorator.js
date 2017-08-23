18400bd61f274bae517222f0bcef3a4aa8c8b9b6
Stephen Kobata <shunter55@gmail.com>
2017-08-22 23:17:58 -0700

    added geneGenorator

--- src/GeneGenorator.js
+++ src/GeneGenorator.js
@@ -7,16 +7,24 @@ const Genome = require('./Genome');
 
 geneConstants = {
 	node = {
+		// Chance that an input gene will be created.
 		input: 0.3,
-		output: 0.3,
-		splitConnection: 0.3
+		// Chance that an output gene will be created.
+		output: 0.3
 	},
 	connection = {
-		create: 0.1
+		// Chance that a connection gene will be created.
+		create: 0.1,
+		// Chance that a connection will be split.
+		splitConnection: 0.3
 	},
-	gene = {
-		maxInputCount = 5,
-		maxOutputCount = 5
+	weight = {
+		// Lower bound of how much a weight mutation can be.
+		changeLower = 0.1;
+		// Upper bound of how much a weight mutation can be.
+		changeUpper = 0.25;
+		// Chance that a weight mutation will occur.
+		chance = 0.3;
 	}
 }
 
@@ -47,10 +55,9 @@ class GeneGenorator {
 
 	/**
 	 *	Create a new genome by mutating the given genome.
-	 *  Mutation may add new:
-	 *		InputNode, OutputNode, Connection
-	 *  Mutation may change:
-	 *		Connection weights
+	 *  Mutation may:
+	 *		add InputNode, add OutputNode, split Connection, add Connection
+	 *      change Connection weights 
 	 *  @return a new genome mutated from the given genome.
 	 */
 	mutateGenome(genome, maxInputs, maxOutputs) {
@@ -73,7 +80,7 @@ class GeneGenorator {
 			newGenome.addConnectionGene(connectionGene);
 		}
 		// If should split connection.
-		if (Math.random() < geneConstants.node.splitConnection) {
+		if (Math.random() < geneConstants.connection.splitConnection) {
 			var connToSplit = geneUtil.getRandomConnection(genome);
 			connToSplit.setEnabled(false);
 
@@ -90,99 +97,29 @@ class GeneGenorator {
 		}
 		// If should create new connection.
 		if (Math.random() < geneConstants.connection.create) {
-			
+			var nodeA = geneUtil.getRandomNode(genome);
+			var nodeB = geneUtil.getRandomNode(genome);
+
+			while(connectionExists(genome, nodeA.id, nodeB.id)) {
+				nodeA = geneUtil.getRandomNode(genome);
+				nodeB = geneUtil.getRandomNode(genome);
+			}
+
+			var newConnection = geneUtil.connection(innovNumber++, nodeA.id, nodeB.id);
+			newGenome.addNodeGene(newConnection);
 		}
 
+		// Mutate weights.
+		newGenome.connections.forEach(function(connection) {
+			// If should mutate weight.
+			if (Math.random() < geneConstants.weight.chance) {
+				connection.weight = Util.randomFloat(geneConstants.weight.changeLower, geneConstants.weight.changeUpper);
+			}
+		});
+
+		return newGenome;
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-	// Add a random node gene.
-	mutateNodeGene(organism) {
-		if (Math.random() < newInputNodeGenePercent) {
-			this.mutateNewInputNodeGene(organism);
-		} else if (Math.random() < newOutputNodeGenePercent) {
-			this.mutateNewOutputNodeGene(organism);
-		} else {
-
-		}
-	}
-
-	// Adds a InputNodeGene to the orgamism's genome.
-	mutateInputGene(genome, numInputs, numOutputs) {
-		// Get output node id.
-		var outId = this.getRandomNodeId(organism.genome);
-
-		// If no node genes exist yet, create the initial one.
-		if (outId == null) {
-			var inNodeGene = this.createInputNodeGene(organism.maxInputCount);
-			var outNodeGene = this.createOutputNodeGene(organism.maxOutputCount);
-			var connectionGene = new ConnectionGene(inNodeGene.id, outNodeGene.id, this.innovNumber++);
-
-			organism.genome.addNodeGene(inNodeGene);
-			organism.genome.addNodeGene(outNodeGene);
-			organism.genome.addConnectionGene(connectionGene);
-		} else {
-			var inNodeGene = this.createInputNodeGene(organism.maxInputCount);
-			var connectionGene = new ConnectionGene(inNodeGene.id, outId, innovNumber++);
-
-			organism.genome.addNodeGene(inNodeGene);
-			organism.genome.addConnectionGene(connectionGene);
-		}
-	}
-
-	mutateNewOutputNodeGene(organism) {
-		// Get input node id.
-		var inId = this.getRandomNodeId(organism.genome);
-
-		var outNodeGene = this.createOutputNodeGene(organism.maxOutputCount);
-		var connectionGene = new ConnectionGene(inId, outNodeGene.id, innovNumber++);
-
-		organism.genome.addNodeGene(outNodeGene);
-		organism.genome.addConnectionGene(connectionGene);
-	}
-
-	mutateNewHiddenNodeGene(organism) {
-		
-	}
-
-
-
-	createRandomNode() {
-
-	}
-
-	createRandomConnection() {
-		// GET in and out. Out cannot be the same as in.
-		var inIdx = Util.random(0, this.nodeCount);
-		var outIdx = Util.random(0, this.nodeCount);
-		while (outIdx == inIdx) {
-			outIdx = Util.random(0, this.nodeCount);
-		}
-		// ADD the new connection.
-		var connection = new Connection(this.nodes[inIdx], this.nodes[outIdx], innovNumber);
-		this.connections.push(connections);
-	}
-
-
-	// Returns a random node id, or null if none exist.
-	getRandomNodeId (genome) {
-		var ids = Object.keys(genome.nodeGenes);
-		if (ids.length == 0) {
-			return null;
-		}
-
-		return ids[Util.random(0, ids.length)];
-	}
 }
 
 var createGene = {
@@ -217,43 +154,17 @@ var geneUtil = {
 	getRandomConnection = function(genome) {
 		var idx = Util.random(0, genome.connectionGenes.length - 1);
 		return genome.connectionGenes[idx];
+	},
+
+	connectionExists = function(genome, inputId, outputId) {
+		genome.connections.forEach(function(connection) {
+			if (connection.inId == inputId && connection.outId == outputId) {
+				return true;
+			}
+			return false;
+		});
 	}
 }
-
-/*
-var genomeUtils = {};
-
-genomeUtils.createInputNodeGene = function (maxInputCount) {
-	if (maxInputCount == null) {
-		maxInputCount = geneConstants.startMaxInputCount;
-	}
-
-	var nodeGene = new NodeGene(nextInnovNumber++, NodeType.input);
-	nodeGene.idx = Util.random(0, maxInputCount);
-	return nodeGene;
-}
-
-genomeUtils.createOutputNodeGene = function (maxOutputCount) {
-	if (maxOutputCount == null) {
-		maxOutputCount = geneConstants.startMaxOutputCount;
-	}
-
-	var nodeGene = new NodeGene(nextInnovNumber++, NodeType.output);
-	nodeGene.idx = Util.random(0, maxOutputCount);
-	return nodeGene;
-}
-
-// Returns a random node id, or null if none exist.
-genomeUtils.getRandomNodeId = function (genome) {
-	var ids = Object.keys(genome.nodeGenes);
-	if (ids.length == 0) {
-		return null;
-	}
-
-	return ids[Util.random(0, ids.length)];
-}
-*/
-
 
 module.exports = GeneGenorator;
 
--- src/Util.js
+++ src/Util.js
@@ -5,8 +5,16 @@ Util = {
 	 */
 	random: function(start, end) {
 		return Math.floor(Math.random() * (end - start + 1)) + start
+	},
+
+	/**
+	 *	Return a random number between decimals start and end inclusive.
+	 */
+	randomFloat: function(start, end) {
+		return Math.random() * (end - start) + start;
 	}
 }
 
 module.exports = Util;
+
 
