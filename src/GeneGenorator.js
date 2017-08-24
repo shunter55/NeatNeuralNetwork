7bb6fc121484ac3d36fa9f36d6c1b26a9f81cb4e
Stephen Kobata <shunter55@gmail.com>
2017-08-23 23:48:51 -0700

    mating

--- src/GeneGenorator.js
+++ src/GeneGenorator.js
@@ -31,6 +31,14 @@ var geneConstants = {
 class GeneGenorator {
 	constructor() {
 		this.innovNumber = 0;
+		this.generationGenes = {};
+	}
+
+	/**
+	 *	Start new generation.
+	 */
+	newGeneration() {
+		this.generationGenes = {};
 	}
 
 	/**
@@ -62,17 +70,17 @@ class GeneGenorator {
 
 		// If should create an input gene.
 		if (Math.random() < geneConstants.node.input) {
-			var inputGene = createGene.input(this.innovNumber++, maxInputs);
+			var inputGene = createGene.input(this, maxInputs);
 			var outGene = geneUtil.getRandomNode(genome);
-			var connectionGene = createGene.connection(this.innovNumber++, inputGene, outputGene);
+			var connectionGene = createGene.connection(this, inputGene, outputGene);
 			newGenome.addNodeGene(inputGene);
 			newGenome.addConnectionGene(connectionGene);
 		}
 		// If should create an output gene.
 		if (Math.random() < geneConstants.node.output) {
-			var outputGene = createGene.output(this.innovNumber++, maxOutputs);
+			var outputGene = createGene.output(this, maxOutputs);
 			var inGene = geneUtil.getRandomNode(genome);
-			var connectionGene = createGene.connection(this.innovNumber++, inGene, outputGene);
+			var connectionGene = createGene.connection(this, inGene, outputGene);
 			newGenome.addNodeGene(outputGene);
 			newGenome.addConnectionGene(connectionGene);
 		}
@@ -81,11 +89,11 @@ class GeneGenorator {
 			var connToSplit = geneUtil.getRandomConnection(genome);
 			connToSplit.setEnabled(false);
 
-			var newNode = createGene.hidden(this.innovNumber++);
+			var newNode = createGene.hidden(this);
 
-			var connA = createGene.connection(this.innovNumber++, connToSplit.inId, newNode.id);
+			var connA = createGene.connection(this, connToSplit.inId, newNode.id);
 			connA.setWeight(1);
-			var connB = createGene.connection(this.innovNumber++, newNode.id, connToSplit.outId);
+			var connB = createGene.connection(this, newNode.id, connToSplit.outId);
 			connB.setWeight(connToSplit.weight);
 
 			newGenome.addNodeGene(newNode);
@@ -97,7 +105,7 @@ class GeneGenorator {
 			var nodeA = geneUtil.getRandomNode(genome);
 			var nodeB = geneUtil.getRandomNode(genome);
 
-			while(connectionExists(genome, nodeA.id, nodeB.id)) {
+			while(geneUtil.connectionExists(genome, nodeA.id, nodeB.id)) {
 				nodeA = geneUtil.getRandomNode(genome);
 				nodeB = geneUtil.getRandomNode(genome);
 			}
@@ -110,10 +118,7 @@ class GeneGenorator {
 		newGenome.connections.forEach(function(connection) {
 			// If should mutate weight.
 			if (Math.random() < geneConstants.weight.chance) {
-				var dir = 1;
-				if (Math.random < 0.5) {
-					dir = -1;
-				}
+				var dir = Math.random() < 0.5 ? 1 : -1;
 				var weightChange = Util.randomFloat(geneConstants.weight.changeLower, geneConstants.weight.changeUpper) * dir;
 				connection.setWeight(connection.weight + weightChange);
 			}
@@ -124,33 +129,76 @@ class GeneGenorator {
 
 	/**
 	 *	Create a new genome by mating two genomes.
+	 *  @return new genome that results from mating the two genomes.
 	 */
 	mateGenomes(genomeA, genomeB, fitnessA, fitnessB) {
+		var newGenome = newGenome();
+		genomeA.nodeGenes.map(function(node) {
+			newGenome.addNodeGene(node);
+		});
+		genomeB.nodeGenes.map(function(node) {
+			newGenome.addNodeGene(node);
+		});
 
+		if (fitnessA == fitnessB) {
+			// Add all disjoint and excess genes.
+			var connGenes = {};
+			genomeA.connectionGenes.map(function(connection) {
+				connGenes[connection.innovNumber] = connection;
+			});
+			genomeB.connectionGenes.map(function(connection) {
+				if (connGenes[connection.innovNum] != undefined && Math.random() > 0.5) {
+					connGenes[connection.innovNumber] = connection;
+				}
+			});
+			Object.keys(connGenes).forEach(function(innovNum) {
+				newGenome.addConnection(connGenes[innovNum]);
+			});
+		} else {
+			// Add disjoint and excess genes from fitter genome only.
+			var strongGenome = fitnessA > fitnessB ? genomeA : genomeB;
+			var weakGenome = fitnessA < fitnessB ? genomeA : genomeB;
+			var connGenes = {};
+			weakGenome.connectionGenes.map(function(connection) {
+				connGenes[connection.innovNum] = connection;
+			});
+			strongGenome.connectionGenes.map(function(connection) {
+				if (connGenes[connection.innovNum] != undefined) {
+					newGenome.addConnection(Math.random() > 0.5 ? connGenes[connection.innovNum] : connection);
+				} else {
+					newGenome.addConnection(connection);
+				}
+			});
+		}
+
+		return newGenome;
 	}
 
 }
 
 var createGene = {
-	input = function(innovNumber, maxInputCount) {
-		var nodeGene = new NodeGene(innovNumber, NodeType.input);
+	input = function(geneGenorator, maxInputCount) {
+		var nodeGene = new NodeGene(geneGenorator.innovNumber++, NodeType.input);
 		nodeGene.idx = Util.randomInt(0, maxInputCount - 1);
 		return nodeGene;
 	},
 
-	output = function(innovNumber, maxOutputCount) {
-		var nodeGene = new NodeGene(innovNumber, NodeType.output);
+	output = function(geneGenorator, maxOutputCount) {
+		var nodeGene = new NodeGene(geneGenorator.innovNumber++, NodeType.output);
 		nodeGene.idx = Util.randomInt(0, maxOutputCount - 1);
 
 		return nodeGene;
 	},
 
-	hidden = function(innovNumber) {
-		return new NodeGene(innovNumber, NodeType.hidden);
+	hidden = function(geneGenorator) {
+		return new NodeGene(geneGenorator.innovNumber++, NodeType.hidden);
 	},
 
-	connection = function(innovNumber, inGene, outGene) {
-		return new ConnectionGene(inGene.id, outGene.id, innovNumber);
+	connection = function(geneGenorator, inGene, outGene) {
+		var innovNum = generation.connectionExists(geneGenorator.generationGenes, inGene.id, outGene.id);
+		innovNum = innovNumber != null ? innovNum : geneGenorator.innovNumber++;
+		generation.addConnection(geneGenorator.generationGenes, inGene.id, outGene.id, innovNum);
+		return new ConnectionGene(inGene.id, outGene.id, innovNum);
 	}
 }
 
@@ -175,6 +223,44 @@ var geneUtil = {
 	}
 }
 
+var generation = {
+	// Adds the innovation number.
+	addConnection = function(generationGenes, inId, outId, innovNumber) {
+		var newConnection = {
+			inId: inId,
+			outId: outId
+		};
+		
+		generationGenes[JSON.stringify(newConnection)] = innovNumber;
+	},
+	// Gets the innovation number of null if gene doesnt exist.
+	connectionExists = function(generationGenes, inId, outId) {
+		var newConnection = {
+			inId: inId,
+			outId: outId
+		};
+
+		return generationGenes[JSON.stringify(newConnection)] != undefined ? generationGenes[JSON.stringify(newConnection)] : null;
+	}
+}
+
 module.exports = GeneGenorator;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
